71f0c98e3adf509bd4647b0f9e5e5c18f61415c8
Simone <simone.sacchi@nubilaria.com>
2018-08-10 14:18:57 +0200

    add dependency

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * this repo needs to import a dependency
+ */
+var uniqueRandomArray = require('unique-random-array');
+
+
+module.exports = {
+    init: function () {
+        console.log("ok");
+    }
+};
